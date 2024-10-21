b3c38d1d3d13b0173bf6d82327594b3547494faf
stefan <stefan.holmberg@systementor.se>
2024-10-21 15:47:23 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+void main() {
+    int n = 1110;
+    if(n > 120){
+        printf("DASASDASDDA");
+    }
+    bool isCool = false;
+    if(isCool){
+        printf("BLA");
+    }
+}
